fdc5b5028c590aae8c696aa3b00b6a439fe55afa
Corinna <wendisch@math.northwestern.edu>
2016-04-28 11:33:04 -0500

    Update graph.py
    Graph seems to work, but example not suitable to see a nice learning curve. (Training set size too small.)

--- modules/graph.py
+++ modules/graph.py
@@ -12,6 +12,7 @@ def get_graph_accuracy_partial(train_set, attribute_metadata, validate_set, nume
     number=len(train_set)*pct
     partial=random.sample(train_set, int(number))
     n= ID3(partial, attribute_metadata, numerical_splits_count, depth)
+    print n.label
     accuracy=validation_accuracy(n, validate_set)
     return accuracy
 
@@ -26,10 +27,13 @@ def get_graph_data(train_set, attribute_metadata, validate_set, numerical_splits
     
 # get_graph will plot the points of the results from get_graph_data and return a graph
 def get_graph(train_set, attribute_metadata, validate_set, numerical_splits_count, depth, iterations, lower, upper, increment):
-    t = np.arange(lower, upper,increment)
+    t = np.arange(lower+increment, upper,increment)
+    print "haha ",len(t)
     data=[]
-    for k in range(0,len(t)+1):
-        data.append(get_graph_data(train_set, attribute_metadata, validate_set, numerical_splits_count, iterations, k))
+    for k in range(0,len(t)):
+        array=get_graph_data(train_set, attribute_metadata, validate_set, numerical_splits_count, iterations, lower+increment)
+        data.append(array[iterations-1])
+        lower=lower+increment
     plt.plot(t, data)
     plt.show()
     
@@ -37,6 +41,6 @@ attribute_metadata = [{'name': "winner",'is_nominal': True},{'name': "opprundiff
 data_set = [[1, 0.27], [0, 0.42], [0, 0.86], [0, 0.68], [0, 0.04], [1, 0.01], [1, 0.33], [1, 0.42], [1, 0.42], [0, 0.51], [1, 0.4]]
 numerical_splits_count = [5, 5]
 val_set = [[1, 0.5],[0,0.3],[0,0.9],[1,0.9],[1,0.27]]
-print get_graph_accuracy_partial(data_set, attribute_metadata, val_set, numerical_splits_count, 0.5)
-print get_graph_data(data_set, attribute_metadata, val_set, numerical_splits_count, 4, 0.5)
-get_graph(data_set, attribute_metadata, val_set, numerical_splits_count, depth, 3, 0,1,0.5)
+#print get_graph_accuracy_partial(data_set, attribute_metadata, val_set, numerical_splits_count, 0.5)
+#print get_graph_data(data_set, attribute_metadata, val_set, numerical_splits_count, 4, 0.5)
+get_graph(data_set, attribute_metadata, val_set, numerical_splits_count, depth, 150, 0,1,0.2)
