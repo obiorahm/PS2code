99ba36483a87a93b100e99e89eb54dceb6e8a75f
obiorahm <obiorahm@gmail.com>
2016-05-06 07:25:10 -0500

    graph included

--- modules/graph.py
+++ modules/graph.py
@@ -1,40 +1,31 @@
 from random import *
 from ID3 import *
-from node import *
 from operator import xor
 from parse import parse
 import matplotlib.pyplot as plt
 import numpy as np
 import os.path
 import random
-from pruning import validation_accuracy
+from pruning import *
 
-depth=0
 
-def get_graph_accuracy_partial(train_set, attribute_metadata, validate_set, numerical_splits_count, pct):
-    number=len(train_set)*pct
-    partial=random.sample(train_set, int(number))
-    n= ID3(partial, attribute_metadata, numerical_splits_count, depth)
-    accuracy=validation_accuracy(n, validate_set)
-    return accuracy
 
-def get_graph_data(train_set, attribute_metadata, validate_set, numerical_splits_count, iterations, pct):
-    array=[]
-    for i in range(1,iterations+1):
-        sum=0
-        for j in range(0,i):
-            sum=sum+get_graph_accuracy_partial(train_set, attribute_metadata, validate_set, numerical_splits_count, pct)
-        array.append(float(sum)/i)
-    return array
-    
-# get_graph will plot the points of the results from get_graph_data and return a graph
+
+
 def get_graph(train_set, attribute_metadata, validate_set, numerical_splits_count, depth, iterations, lower, upper, increment):
-    t = np.arange(lower+increment, upper,increment)
-    data=[]
-    for k in range(0,len(t)):
-        array=get_graph_data(train_set, attribute_metadata, validate_set, numerical_splits_count, iterations, lower+increment)
-        data.append(array[iterations-1])
-        lower=lower+increment
-    plt.plot(t, data)
+    tree_acc = []
+    t = []
+
+    for i in range(0,100, 20):
+        split_count = [numerical_splits_count[1]] * len(numerical_splits_count)
+        number=float(len(train_set))*(float(i)/100.0)
+        partial=random.sample(train_set, int(number))   
+        n = ID3(partial, attribute_metadata, split_count, depth)
+        x = validation_accuracy(n, validate_set)
+        print number, x
+        tree_acc.append(x)
+        t.append(number)
+    plt.plot(t,tree_acc)
     plt.show()
+    
     
